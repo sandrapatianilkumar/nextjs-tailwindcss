1b9e658295d24a0b2ab05c6d5731ac80df206a3e
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2023-01-21 23:46:41 +0530

    layers -global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '976px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      spacing:{
+        '1':'2px',
+        '2':'16px',
+        '3':'24px',
+        '4':'32px',
+        '5':'48px'
+      },
       colors:{
         vuejs:{
           100:"#49e695",
