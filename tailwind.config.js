44bb54cab660607a25266137c51a352696989ccb
sandrapatianilkumar <sandrapati.anilkumar@gmail.com>
2023-01-21 23:15:07 +0530

    nextjs-tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        vuejs:{
+          100:"#49e695",
+          932:"#000000"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
